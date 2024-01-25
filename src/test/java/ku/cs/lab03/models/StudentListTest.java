26ee731b37f5291beef97315e197bb59e2fca345
plbkppexo <worapha.c@ku.th>
2024-01-25 12:39:35 +0700

    testMethod

--- src/test/java/ku/cs/lab03/models/StudentListTest.java
+++ src/test/java/ku/cs/lab03/models/StudentListTest.java
@@ -1,4 +1,29 @@
+package ku.cs.lab03.models;
+
+import ku.cs.lab03.services.Datasource;
+import ku.cs.lab03.services.StudentHardCodeDatasource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StudentListTest {
-  
+
+    private StudentList studentList;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        Datasource<Student> datasource = new StudentHardCodeDatasource();
+        studentList = datasource.readData();
+    }
+
+    @Test
+    void testAddOneStudent() {
+        int oldsize = studentList.getStudent().size();
+        studentList.addNewStudent("123", "ABC");
+        ArrayList<Student> stdArr = studentList.getStudents();
+    }
+
 }
--- src/test/java/ku/cs/lab03/models/StudentTest.java
+++ src/test/java/ku/cs/lab03/models/StudentTest.java
@@ -1,4 +1,33 @@
+package ku.cs.lab03.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StudentTest {
-  
+    private Student student;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        student = new Student("123", "ABC");
+    }
+
+    @Test
+    @DisplayName("ทดสอบการเพิ่มคะแนน 30 คะแนน")
+    void testAddScore(){
+        Student student = new Student("123", "ABC");
+        student.addScore(30);
+        assertEquals(30, student.getScore());
+
+    }
+
+    @Test
+    @DisplayName("ทดสอบคำนวนเกรด F")
+    void testCalculateGrade(){
+        Student student = new Student("123", "ABC");
+        assertEquals("F", student.grade());
+    }
+
 }
